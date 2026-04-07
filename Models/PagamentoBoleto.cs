9b8ccedca943c76b48ba70149a23dad0420ec2df
mneugebauer-integration <mneugebauer@integrationconsulting.com.br>
2026-04-07 13:41:16 -0300

    feat: implementação do sistema de pagamento com cartão e boleto

---
+++ Models/PagamentoBoleto.cs
@@ -0,0 +1,9 @@
+﻿namespace sistemaDePagamento.Models;
+
+public class PagamentoBoleto : Pagamento {
+  public string CodigoBarras { get; set; } = string.Empty;
+
+  public override string ProcessarPagamento() {
+    return $"Processando pagamento de R$ {Valor:N2} via Boleto (Cod Barra: {CodigoBarras}) na data {DataPagamento:dd/MM/yyyy}.";
+  }
+}
